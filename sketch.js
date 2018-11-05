715b24cb7127546e7fb39d0b0895a27d397c6569
rachelebarazzetta <rachele.baraz@gmail.com>
2018-11-05 18:55:29 +0100

    main structure

--- sketch.js
+++ sketch.js
@@ -1,39 +1,57 @@
 var video;
-var snapshost = [];
-
-function preload(){
-  // put preload code here
-}
+var button;
+var snapshots = [];
+var counter = 0;
+var total = 86;
+var mic;
 
 function setup() {
-  // put setup code here
-  createCanvas(800, 240);
-  video = createCapture(VIDEO);
-  video.size(340, 240);
-  video.hide();
-
-  background('orange');
-
-  button = createButton('take a photo');
-  button.mousePressed(takesnap);
+  createCanvas(1360, 630);
+  background(52);
+  video = createCapture(VIDEO, ready);
+  video.size(0, 0);
+  imageMode(CORNER);
+  frameRate(20000);
+  textAlign(CENTER);
+  fill('white');
+  text('It works only on crome!', width/2, height/2);
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
-function takesnap(){
-  // snapshost.push(video.get());
-  for(var i = 0; i = snapshost.lenght; i++){
-    tint(255, 50);
-    image(snapshost[i], 0, 0, 340, 240);
-    var snapshost = video.loadPixels();
-    console.log('ciao');
-  }
+var go = false;
+
+function ready(){
+  go = true;
 }
 
 function draw() {
-  // put drawing code here
-
-
+  if(go){
+  snapshots[counter] = video.get();
+  counter++;
+  if(counter == total ){
+    counter = 0;
+  }
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+  var w = 340;
+  var h = 180;
+  var x = 0;
+  var y = 0;
+
+  var  vol = mic.getLevel();
+
+  for(var i = 0; i < snapshots.length ; i++){
+    var index = (i + frameCount) % snapshots.length ;
+    tint( 0, vol*2550, 0);
+    image(snapshots[index], x , y, w , h);
+    x = x + w/2;
+
+    if (x > width){
+      x = 0;
+      y = y + h/2;
+    }
+  }
+
+
 }
